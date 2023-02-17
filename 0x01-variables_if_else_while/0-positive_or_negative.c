624ae21f2edb161ed0449e045d8e733c4332422f
thegirlharvey <graciemughogho@gmail.com>
2023-02-17 12:23:17 -0800

    Random number program

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,10 +14,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-		printf("%lu is positive\n", n);
+		printf("%i is positive\n", n);
 	if (n < 0)
-		printf("%lu is negative\n", n);
+		printf("%i is negative\n", n);
 	if (n == 0)
-		printf("%lu is zero\n", n);
+		printf("%i is zero\n", n);
 	return (0);
 }
