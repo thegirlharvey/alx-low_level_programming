07f57fcbb4b86778461e24483b2e8919c7509d1d
thegirlharvey <graciemughogho@gmail.com>
2023-02-17 08:54:20 -0800

    Printf size of types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+
+	printf("Size of char: %zu byte(s)\n", sizeof(a));
+	printf("Size of int: %zu byte(s)\n", sizeof(b));
+	printf("Size of long int: %zu byte(s)\n", sizeof(c));
+	printf("Size of long long int: %zu byte(s)\n", sizeof(d));
+	printf("Size of float: %zu byte(s)\n", sizeof(e));
+
+	return (0);
+}
