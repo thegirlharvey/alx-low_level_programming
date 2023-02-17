419c908f58491826b0d6eacb9b82affb06a0816c
thegirlharvey <graciemughogho@gmail.com>
2023-02-17 10:27:55 -0800

    Print without printf

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Return: 1
+ */
+int main(void)
+{
+	system("echo 'and that piece of art is useful\" - Dora Korpar, 2015-10-19'");
+
+	return (1);
+}
