73d967f1e55b72124eebe3ab279367fa28499230
thegirlharvey <graciemughogho@gmail.com>
2023-02-17 17:40:58 -0800

    Print alphabet backwards with putchar

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet backwards
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c > 'a'; --a)
+		putchar(c);
+	putchar('\n');
+	return (0);
+
+}
