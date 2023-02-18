1922fa65b3f2bfeab66d148d23647416ff735af5
thegirlharvey <graciemughogho@gmail.com>
2023-02-18 11:52:34 -0800

    Print possible combinations of tripple digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - prints possible combinations of triple digits
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, b, c;
+
+	for (a = 0; a < 9; a++)
+	for (b = a + 1; b < 9; b++)
+	for (c = b + 1; c < 10; c++)
+	{
+		putchar(a + '0');
+		putchar(b + '0');
+		putchar(c + '0');
+
+		if (a == 7 && b == 8 && c == 9)
+			continue;
+		if (c < 9)
+			putchar(',');
+			putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
