445ef09bf9635b8d9987772c7de39468797dae43
thegirlharvey <graciemughogho@gmail.com>
2023-02-17 16:37:09 -0800

    Print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; ++n)
+	printf("%i");
+	printf("\n");
+	return (0);
+}
