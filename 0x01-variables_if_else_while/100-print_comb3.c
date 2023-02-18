0d9217b42a3fe67242e35c1f7587bbd354cc36b9
thegirlharvey <graciemughogho@gmail.com>
2023-02-18 09:23:36 -0800

    Print combination numbers

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,10 +11,10 @@ int main(void)
 
 	for (a = 0; a < 10; a++)
 	for (b = a + 1; b < 10; b++)
-		putchar(a + '0');
-		putchar(b + '0');
-			putchar(',');
-			putchar(' ');
+	putchar(a + '0');
+	putchar(b + '0');
+		putchar(',');
+		putchar(' ');
 	putchar('\n');
 	return (0);
 }
