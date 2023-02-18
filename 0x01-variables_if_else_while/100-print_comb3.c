12cf7ed92987d41cf5c3886037cc2d54c240ba74
thegirlharvey <graciemughogho@gmail.com>
2023-02-18 09:14:23 -0800

    Print combination numbers

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints different combs on double digits
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = 0; a < 10; a++)
+		for (b = a + 1; b < 10; b++)
+			if (a != b && b != a)
+				putchar(a + '0');
+				putchar(b + '0');
+					putchar(',');
+					putchar(' ');
+	putchar('\n');
+	return (0);
+}
