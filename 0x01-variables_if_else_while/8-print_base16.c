6eecf21458386e3b430b385265dd56887c837cab
thegirlharvey <graciemughogho@gmail.com>
2023-02-17 18:10:02 -0800

    Print without printf

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,9 +8,12 @@
 int main(void)
 {
 	int n;
+	char a;
 
-	for (n = 0; n <= 16; ++n)
+	for (n = 0; n <= 10; ++n)
 		putchar('0' | n);
+	for (a = 'a'; a <= 'f'; ++a)
+		putchar(a);
 	putchar('\n');
 	return (0);
 }
