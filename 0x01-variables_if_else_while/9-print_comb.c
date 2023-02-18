f7b721b05969b1ab93f54f8fbf77a64e94a9fb55
thegirlharvey <graciemughogho@gmail.com>
2023-02-17 18:38:22 -0800

    Print without digitd with,

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers with ,
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; ++n)
+		putchar('0' | n);
+	putchar(',');
+	putchar(' ');
+	return (0);
+}
