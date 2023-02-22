76c93bfbaa1579b06202721d66aabafc11bb2ce0
thegirlharvey <graciemughogho@gmail.com>
2023-02-22 12:57:12 -0800

    Printing alphabet 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,9 +10,8 @@ void print_alphabet_x10(void)
 	char a;
 	int b = 0;
 
-	while (b++ <= 10)
+	while (b++ <= 9)
 		for (a = 'a'; a <= 'z'; a++)
 			_putchar(a);
-
-	_putchar('\n');
+		_putchar('\n');
 }
