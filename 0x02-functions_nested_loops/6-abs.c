bcb017009d48aab8b6480d14756d2584799dace6
thegirlharvey <graciemughogho@gmail.com>
2023-02-22 19:29:41 -0800

    Program checks for absolute int

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - computes absolute value of an int
+ *@n: interger to check
+ *
+ * Return: absolute value on integer
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+
+}
