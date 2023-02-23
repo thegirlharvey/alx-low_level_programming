48e945d505e69147bd114c9979e5db7a742f4b32
thegirlharvey <graciemughogho@gmail.com>
2023-02-23 04:05:40 -0800

    print natural numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,6 +8,9 @@ void print_to_98(int n)
 {
 	int i;
 
-	for (i = n; i > 98; i--)
-		printf("%d\n", i);
+	for (i = n; i >= 98; i--)
+		printf("%d, ", i);
+
+	for (i = n; i < 98; i++)
+		printf("%d, ", i);
 }
